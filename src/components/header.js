65793ff6661dac55c5ffef55a4e7512c4b9506f8
Tyler Wray <wraytw@gmail.com>
2019-02-18 20:44:22 -0700

    More setup, styling site

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,112 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import profile from "../images/profile.png"
 
-Header.propTypes = {
-  siteTitle: PropTypes.string
-}
+import MobileNav from "./MobileNav"
 
-Header.defaultProps = {
-  siteTitle: ``
+const Main = styled.header`
+  padding-bottom: 5rem;
+  background-image: linear-gradient(var(--green), var(--cream));
+`
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.5rem 2rem;
+`
+
+const Title = styled.h1`
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const TitleLink = styled(Link)`
+  color: var(--black);
+  text-decoration: none;
+  font-family: Satisfy;
+  z-index: var(--index-level-3);
+`
+
+const HamburgerButton = styled.button`
+  padding: 24px;
+  background: transparent;
+  border: none;
+  outline: none;
+`
+
+const Hamburger = styled.div`
+  width: 24px;
+  height: 2px;
+  position: relative;
+  left: 0px;
+  background: ${props => (props.open ? "transparent" : "var(--black)")};
+  transition: all 250ms cubic-bezier(0.86, 0, 0.07, 1) 0s;
+  cursor: pointer;
+  z-index: var(--index-level-3);
+
+  &::before {
+    content: "";
+    top: ${props => (props.open ? 0 : "-8px")};
+    width: 24px;
+    height: 2px;
+    position: absolute;
+    left: 0px;
+    transform: rotate(${props => (props.open ? 45 : 0)}deg);
+    background: var(--black);
+    transition: all 250ms cubic-bezier(0.86, 0, 0.07, 1) 0s;
+  }
+
+  &::after {
+    top: ${props => (props.open ? 0 : "8px")};
+    content: "";
+    width: 24px;
+    height: 2px;
+    position: absolute;
+    left: 0px;
+    transform: rotate(${props => (props.open ? -45 : 0)}deg);
+    background: var(--black);
+    transition: all 250ms cubic-bezier(0.86, 0, 0.07, 1) 0s;
+  }
+`
+
+const ProfileImage = styled.div`
+  display: block;
+  visibility: visible;
+  width: 160px;
+  height: 160px;
+  margin: 0 auto;
+  border-radius: 50%;
+  overflow: hidden;
+  background-size: 100%;
+  background: url(${profile}) 5px / cover no-repeat var(--green);
+  background-repeat: no-repeat;
+`
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <Main>
+      <Container>
+        <Title>
+          <TitleLink to="/">Tyler Wray</TitleLink>
+          <HamburgerButton onClick={toggleMenu}>
+            <Hamburger open={menuOpen} />
+          </HamburgerButton>
+        </Title>
+      </Container>
+      <ProfileImage />
+      <MobileNav open={menuOpen} />
+    </Main>
+  )
 }
 
 export default Header
---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const About = () => (
+  <Layout>
+    <SEO
+      title="About Tyler Wray"
+      keywords={["tyler wray", "about", "family", "hobbies"]}
+    />
+    <div>Hi</div>
+  </Layout>
+)
+
+export default About
