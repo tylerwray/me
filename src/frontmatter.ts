686f7407bd015fa98f1be164d92f704007947a81
Tyler Wray <wraytw@gmail.com>
2022-12-08 20:58:16 -0700

    Tutorials section broken out!

--- src/frontmatter.ts
+++ src/frontmatter.ts
@@ -5,13 +5,23 @@ interface FrontmatterImage {
   creditUrl: string;
 }
 
+export type Tag =
+  | "elixir"
+  | "phoenix"
+  | "javascript"
+  | "react"
+  | "gatsby"
+  | "mdx"
+  | "typescript"
+  | "tailwind";
+
 export interface Frontmatter {
   // Injected from plugin
   minutesToRead: number;
   title: string;
   description: string;
-  draft?: boolean
-  tags?: string[];
+  draft?: boolean;
+  tags?: Tag[];
   publishedOn: string;
   image: FrontmatterImage;
 }
